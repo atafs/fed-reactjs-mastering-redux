050e8fd9dd178b4eb83d6de0e384772810793a34
atafs <americoviagens@gmail.com>
2017-09-07 15:17:36 +0100

    23: Adding a second reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import CharacterList from './CharacterList';
+import HeroList from './HeroList';
+import SquadStats from './SquadStats';
+
+import '../styles/index.css';
+
 class App extends Component {
     render() {
         return (
-            <div>
-                <h2>Supersquad</h2>
+            <div className="App">
+                <h2><strong>SuperSquad</strong></h2>
+                <div className="row">
+                    <div className="col">
+                        <CharacterList />
+                    </div>
+                    <div className="col">
+                        <HeroList />
+                    </div>
+                    <div className="col">
+                        <SquadStats />
+                    </div>
+                </div>
             </div>
         ) 
     }
